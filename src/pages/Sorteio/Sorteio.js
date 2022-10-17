cc52be4a844e549d12f91787dc55ab4ef3930817
JOAO MARCOS <joaomgsa@gmail.com>
2022-10-17 10:38:49 -0300

    Add files via upload

--- src/pages/Sorteio/Sorteio.js
+++ src/pages/Sorteio/Sorteio.js
@@ -19,15 +19,8 @@ export default function Sorteio(){
       const[paises, setPaises] = useState([]);
 
       useEffect(()=> {
-        async function loadPaises(){
-            const url = "https://estagio.geopostenergy.com/WorldCup/GetAllTeams";
-            const response = await fetch(url,
-                {
-                    method: 'get',
-                    headers: {'git-user': 'joaomgsa'}
-                })
-                .then(response=> response.json())
-                const resposta = {"Status":200,"Result":[{"Token":"82d9e5fd-edc4-494b-a42b-6f0f8847a77b","Name":"Qatar"},{"Token":"b18e8f55-3477-4d76-bb1a-811132eb25fc","Name":"Equador"}],"Error":false};
+         function loadPaises(){
+                const resposta = api 
                 const respostapaises = resposta.Result;
                 setPaises(respostapaises);
         }
@@ -41,29 +34,35 @@ export default function Sorteio(){
     
 
     return(
-        <main>
+        <div>
+            <Container>
+                <h2>Paises Classificados</h2>
+                <Table>
+                   <thead>
+                        <th>Pais</th>
+                   </thead> 
+                   <tbody>
+                        {paises.map(pais => (
+                            <tr key={pais.Token}>
+                                <td>{pais.Name}</td>
+                            </tr>
+                        ))}
+                   </tbody>
+                </Table>
+                <a className="button" href="\grupos">exibir grupos</a>
+            </Container>
+        </div>
+
+
+
+        /*<main>
             <ul>
                 {paises.map(pais =>(
                     <li key={pais.Token}>{pais.Name}</li>
                     ))}
             </ul>
-         </main>
+         </main>*/
     );
 }
 
 
-/* <div>
-            <Container>
-            <h2>Paises classificados</h2>
-                <Table>
-                    <thead>
-                        <th>Pais</th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{paises.map((pais)=><tr>{pais.Name}</tr>)}</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </Container>    
-        </div> */
